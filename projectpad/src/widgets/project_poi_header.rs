53454b545157f31779f36b184465d30febc8c546
Emmanuel Touzery <etouzery@gmail.com>
2020-06-26 15:07:31 +0200

    split 'populate_grid' out for reuse

--- projectpad/src/widgets/project_poi_header.rs
+++ projectpad/src/widgets/project_poi_header.rs
@@ -18,6 +18,89 @@ pub struct Model {
     title: gtk::Label,
 }
 
+pub struct GridItem {
+    pub label_name: &'static str,
+    pub icon: Option<Icon>,
+    pub markup: String,
+    pub raw_value: String,
+}
+
+impl GridItem {
+    pub fn new(
+        label_name: &'static str,
+        icon: Option<Icon>,
+        markup: String,
+        raw_value: String,
+    ) -> GridItem {
+        GridItem {
+            label_name,
+            icon,
+            markup,
+            raw_value,
+        }
+    }
+}
+
+pub fn populate_grid(
+    header_grid: gtk::Grid,
+    actions_popover: gtk::Popover,
+    fields: &[GridItem],
+    register_btn: &dyn Fn(gtk::ModelButton, String),
+) {
+    for child in header_grid.get_children() {
+        header_grid.remove(&child);
+    }
+    for child in actions_popover.get_children() {
+        actions_popover.remove(&child);
+    }
+    let popover_vbox = gtk::BoxBuilder::new()
+        .margin(10)
+        .orientation(gtk::Orientation::Vertical)
+        .build();
+    popover_vbox.add(&gtk::ModelButtonBuilder::new().label("Edit").build());
+
+    let mut i = 0;
+    for item in fields {
+        let label = gtk::LabelBuilder::new().label(&item.label_name).build();
+        header_grid.attach(&label, 0, i, 1, 1);
+        label.get_style_context().add_class("item_label");
+
+        let label = gtk::LabelBuilder::new()
+            .use_markup(true)
+            .label(&item.markup)
+            .xalign(0.0)
+            .build();
+
+        if let Some(icon) = &item.icon {
+            let gbox = gtk::BoxBuilder::new().build();
+            // https://github.com/gtk-rs/gtk/issues/837
+            // property_icon_size: 1, // gtk::IconSize::Menu,
+            gbox.add(
+                &gtk::ImageBuilder::new()
+                    .icon_name(&icon.name())
+                    .icon_size(1)
+                    .margin_end(5)
+                    .build(),
+            );
+            gbox.add(&label);
+            header_grid.attach(&gbox, 1, i, 1, 1);
+        } else {
+            header_grid.attach(&label, 1, i, 1, 1);
+        }
+
+        let popover_btn = gtk::ModelButtonBuilder::new()
+            .label(&format!("Copy {}", item.label_name))
+            .build();
+        register_btn(popover_btn.clone(), item.raw_value.clone());
+        popover_vbox.add(&popover_btn);
+
+        i += 1;
+    }
+    header_grid.show_all();
+    popover_vbox.show_all();
+    actions_popover.add(&popover_vbox);
+}
+
 #[widget]
 impl Widget for ProjectPoiHeader {
     fn init_view(&mut self) {
@@ -102,22 +185,16 @@ impl Widget for ProjectPoiHeader {
     }
 
     fn populate_header(&self) {
-        for child in self.header_grid.get_children() {
-            self.header_grid.remove(&child);
-        }
-        for child in self.model.header_popover.get_children() {
-            self.model.header_popover.remove(&child);
-        }
         let fields = match &self.model.project_item {
             Some(ProjectItem::Server(srv)) => vec![
-                (
+                GridItem::new(
                     "Address",
                     Some(Self::server_access_icon(&srv)),
                     Self::server_ip_display(&srv),
                     srv.ip.clone(),
                 ),
-                ("Username", None, srv.username.clone(), srv.username.clone()),
-                (
+                GridItem::new("Username", None, srv.username.clone(), srv.username.clone()),
+                GridItem::new(
                     "Password",
                     None,
                     if srv.password.is_empty() {
@@ -129,63 +206,24 @@ impl Widget for ProjectPoiHeader {
                 ),
             ],
             Some(ProjectItem::ProjectPointOfInterest(poi)) => vec![
-                ("Path", None, poi.path.clone(), poi.path.clone()),
-                ("Text", None, poi.text.clone(), poi.path.clone()),
+                GridItem::new("Path", None, poi.path.clone(), poi.path.clone()),
+                GridItem::new("Text", None, poi.text.clone(), poi.path.clone()),
             ],
             _ => vec![],
         };
-
-        let popover_vbox = gtk::BoxBuilder::new()
-            .margin(10)
-            .orientation(gtk::Orientation::Vertical)
-            .build();
-        popover_vbox.add(&gtk::ModelButtonBuilder::new().label("Edit").build());
-
-        let mut i = 0;
-        for (name, maybe_icon, markup, rawval) in fields {
-            let label = gtk::LabelBuilder::new().label(name).build();
-            self.header_grid.attach(&label, 0, i, 1, 1);
-            label.get_style_context().add_class("item_label");
-
-            let label = gtk::LabelBuilder::new()
-                .use_markup(true)
-                .label(&markup)
-                .xalign(0.0)
-                .build();
-
-            if let Some(icon) = maybe_icon {
-                let gbox = gtk::BoxBuilder::new().build();
-                // https://github.com/gtk-rs/gtk/issues/837
-                // property_icon_size: 1, // gtk::IconSize::Menu,
-                gbox.add(
-                    &gtk::ImageBuilder::new()
-                        .icon_name(&icon.name())
-                        .icon_size(1)
-                        .margin_end(5)
-                        .build(),
+        populate_grid(
+            self.header_grid.clone(),
+            self.model.header_popover.clone(),
+            &fields,
+            &|btn: gtk::ModelButton, str_val: String| {
+                relm::connect!(
+                    self.model.relm,
+                    btn,
+                    connect_clicked(_),
+                    Msg::CopyClicked(str_val.clone())
                 );
-                gbox.add(&label);
-                self.header_grid.attach(&gbox, 1, i, 1, 1);
-            } else {
-                self.header_grid.attach(&label, 1, i, 1, 1);
-            }
-
-            let popover_btn = gtk::ModelButtonBuilder::new()
-                .label(&format!("Copy {}", name))
-                .build();
-            relm::connect!(
-                self.model.relm,
-                popover_btn,
-                connect_clicked(_),
-                Msg::CopyClicked(rawval.clone())
-            );
-            popover_vbox.add(&popover_btn);
-
-            i += 1;
-        }
-        self.header_grid.show_all();
-        popover_vbox.show_all();
-        self.model.header_popover.add(&popover_vbox);
+            },
+        );
     }
 
     view! {

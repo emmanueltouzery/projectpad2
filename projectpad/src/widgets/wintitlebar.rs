d404ebba9b5d6ede442335618d1419f5d558518b
Emmanuel Touzery <etouzery@gmail.com>
2020-06-27 20:08:36 +0200

    GUI progress on the search entry

--- projectpad/src/widgets/wintitlebar.rs
+++ projectpad/src/widgets/wintitlebar.rs
@@ -3,7 +3,10 @@ use relm::Widget;
 use relm_derive::{widget, Msg};
 
 #[derive(Msg)]
-pub enum Msg {}
+pub enum Msg {
+    SearchClicked,
+    SearchChanged,
+}
 
 pub struct Model {}
 
@@ -15,13 +18,48 @@ impl Widget for WinTitleBar {
         Model {}
     }
 
-    fn update(&mut self, event: Msg) {}
+    fn update(&mut self, event: Msg) {
+        match event {
+            Msg::SearchClicked => {
+                let new_visible = self.search_toggle.get_active();
+                self.search_entry.set_visible(new_visible);
+                if new_visible {
+                    self.search_entry.grab_focus();
+                }
+            }
+            Msg::SearchChanged => {
+                println!(
+                    "{}",
+                    self.search_entry
+                        .get_text()
+                        .map(|t| t.to_string())
+                        .unwrap_or_else(|| "".to_string())
+                );
+            }
+        }
+    }
 
     view! {
         #[name="header_bar"]
         gtk::HeaderBar {
             show_close_button: true,
             title: Some("Projectpad"),
+            #[name="search_toggle"]
+            gtk::ToggleButton {
+                image: Some(&gtk::Image::new_from_icon_name(Some("edit-find-symbolic"), gtk::IconSize::Menu)),
+                child: {
+                    pack_type: gtk::PackType::End
+                },
+                toggled() => Msg::SearchClicked
+            },
+            #[name="search_entry"]
+            gtk::Entry {
+                visible: false,
+                child: {
+                    pack_type: gtk::PackType::End
+                },
+                changed() => Msg::SearchChanged
+            },
         }
     }
 }

18d73f91b581724f29992bbc016769f7c46a4199
Emmanuel Touzery <etouzery@gmail.com>
2020-08-03 12:33:34 +0200

    progress, new features not working yet, but i think i'll drop this & move to pulldown-cmark

--- projectpad/src/notes.rs
+++ projectpad/src/notes.rs
@@ -1,12 +1,18 @@
 use nom::branch::alt;
 use nom::bytes::complete::*;
 use nom::combinator::*;
-#[cfg(test)]
-use nom::multi::many0;
-use nom::multi::many_till;
+use nom::multi::{many0, many1, many_till};
 use nom::IResult;
 
 #[derive(Debug, PartialEq, Eq)]
+enum NoteElementNoBlockQuote<'a> {
+    Header1(&'a str),
+    Header2(&'a str),
+    Header3(&'a str),
+    Paragraph(Vec<LineItem<'a>>),
+}
+
+#[derive(Debug, PartialEq, Eq, Clone)]
 enum LineItem<'a> {
     Bold(Vec<LineItem<'a>>),
     Italics(Vec<LineItem<'a>>),
@@ -26,6 +32,30 @@ fn bold(input: &str) -> IResult<&str, LineItem> {
 
 fn italics(input: &str) -> IResult<&str, LineItem> {
     text_toggle(input, "*", LineItem::Italics)
+}
+
+// eat simple carriage returns before & after headers.
+fn headers(input: &str) -> IResult<&str, NoteElementNoBlockQuote> {
+    let (input, _) = many0(is_a("\n\r"))(input)?;
+    let (input, o) = alt((
+        header("###", NoteElementNoBlockQuote::Header3),
+        header("##", NoteElementNoBlockQuote::Header2),
+        header("#", NoteElementNoBlockQuote::Header1),
+    ))(input)?;
+    let (input, _) = many0(is_a("\n\r"))(input)?;
+    Ok((input, o))
+}
+
+fn header<'a>(
+    tag_val: &'static str,
+    ctor: impl Fn(&'a str) -> NoteElementNoBlockQuote<'a>,
+) -> impl Fn(&'a str) -> IResult<&'a str, NoteElementNoBlockQuote<'a>> {
+    move |input| {
+        let (input, _) = tag(format!("{} ", tag_val).as_str())(input)?;
+        let (input, o) = is_not("\r\n")(input)?;
+        let (input, _) = is_a("\r\n")(input)?;
+        Ok((input, ctor(o)))
+    }
 }
 
 fn text_toggle<'a>(
@@ -49,6 +79,77 @@ fn link(input: &str) -> IResult<&str, LineItem> {
 
 fn line_item(input: &str) -> IResult<&str, LineItem> {
     alt((plaintext, bold, italics, link))(input)
+}
+
+fn paragraph(input: &str) -> IResult<&str, NoteElementNoBlockQuote> {
+    // the haskell parser included endOfInput https://github.com/emmanueltouzery/projectpad/blob/master/src/Notes.hs#L100
+    let (input, (items, _)) = many_till(
+        line_item,
+        alt((
+            end_of_paragraph,
+            upcoming_preformat,
+            upcoming_blockquote,
+            upcoming_headers,
+        )),
+    )(input)?;
+    Ok((
+        input,
+        // NoteElementNoBlockQuote::Paragraph(merge_plaintexts(&items)),
+        NoteElementNoBlockQuote::Paragraph(items),
+    ))
+}
+
+// fn merge_plaintexts<'a, 'b>(items: &'b [LineItem<'a>]) -> Vec<LineItem<'a>> {
+//     items
+//         .iter()
+//         .fold((vec![], None), |mut sofar, cur| match (sofar.1, cur) {
+//             (Some(LineItem::PlainText(a)), LineItem::PlainText(b)) => {
+//                 (sofar.0, Some(LineItem::PlainText(&(a.to_string() + b))))
+//             }
+//             (Some(LineItem::PlainText(a)), _) => {
+//                 sofar.0.push(sofar.1.unwrap());
+//                 sofar
+//             }
+//             (None, p @ LineItem::PlainText(_)) => (sofar.0, Some(p.clone())),
+//             (None, other) => {
+//                 sofar.0.push(other.clone());
+//                 sofar
+//             }
+//         })
+//         .0 // !!!!!
+// }
+
+fn end_of_paragraph(input: &str) -> IResult<&str, ()> {
+    let (input, _) = is_a("\r\n")(input)?;
+    let (input, _) = is_a("\t ")(input)?;
+    let (input, _) = is_a("\r\n")(input)?;
+    Ok((input, ()))
+}
+
+fn upcoming_preformat(input: &str) -> IResult<&str, ()> {
+    let (input, _) = is_a("\r\n")(input)?;
+    let (input, _) = peek(tag("    "))(input)?;
+    Ok((input, ()))
+}
+
+fn upcoming_blockquote(input: &str) -> IResult<&str, ()> {
+    let (input, _) = is_a("\r\n")(input)?;
+    let (input, _) = peek(tag("> "))(input)?;
+    Ok((input, ()))
+}
+
+fn upcoming_headers(input: &str) -> IResult<&str, ()> {
+    let (input, _) = is_a("\r\n")(input)?;
+    let (input, _) = peek(headers)(input)?;
+    Ok((input, ()))
+}
+
+fn note_element(input: &str) -> IResult<&str, NoteElementNoBlockQuote> {
+    alt((headers, paragraph))(input)
+}
+
+fn note_document(input: &str) -> IResult<&str, Vec<NoteElementNoBlockQuote>> {
+    many1(note_element)(input)
 }
 
 #[test]
@@ -111,4 +212,12 @@ fn parse_link_with_rich_contents() {
             ]
         ))
     )
+}
+
+#[test]
+fn parse_note_document_simple_header() {
+    assert_eq!(
+        note_document("# hello world"),
+        Ok(("", vec![NoteElementNoBlockQuote::Header1("hello world")]))
+    );
 }

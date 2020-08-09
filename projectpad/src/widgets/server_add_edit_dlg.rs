af71231e5446423b971c462a0891cf51f2d5a574
Emmanuel Touzery <etouzery@gmail.com>
2020-08-09 16:24:39 +0200

    server edit: is retired+server type

--- projectpad/src/widgets/server_add_edit_dlg.rs
+++ projectpad/src/widgets/server_add_edit_dlg.rs
@@ -1,5 +1,5 @@
 use gtk::prelude::*;
-use projectpadsql::models::{Server, ServerAccessType};
+use projectpadsql::models::{Server, ServerAccessType, ServerType};
 use relm::Widget;
 use relm_derive::{widget, Msg};
 
@@ -9,15 +9,32 @@ pub enum Msg {}
 pub struct Model {
     relm: relm::Relm<ServerAddEditDialog>,
     description: String,
+    is_retired: bool,
     address: String,
     text: String,
     username: String,
     password: String,
+    server_type: ServerType,
 }
 
 #[widget]
 impl Widget for ServerAddEditDialog {
-    fn init_view(&mut self) {}
+    fn init_view(&mut self) {
+        self.server_type
+            .append(Some(&ServerType::SrvApplication.to_string()), "Application");
+        self.server_type
+            .append(Some(&ServerType::SrvDatabase.to_string()), "Database");
+        self.server_type.append(
+            Some(&ServerType::SrvHttpOrProxy.to_string()),
+            "HTTP server or proxy",
+        );
+        self.server_type
+            .append(Some(&ServerType::SrvMonitoring.to_string()), "Monitoring");
+        self.server_type
+            .append(Some(&ServerType::SrvReporting.to_string()), "Reporting");
+        self.server_type
+            .set_active_id(Some(&self.model.server_type.to_string()));
+    }
 
     // TODO probably could take an Option<&Server> and drop some cloning
     fn model(relm: &relm::Relm<Self>, server: Option<Server>) -> Model {
@@ -27,6 +44,7 @@ impl Widget for ServerAddEditDialog {
                 .as_ref()
                 .map(|s| s.desc.clone())
                 .unwrap_or_else(|| "".to_string()),
+            is_retired: server.as_ref().map(|s| s.is_retired).unwrap_or(false),
             address: server
                 .as_ref()
                 .map(|s| s.ip.clone())
@@ -43,6 +61,10 @@ impl Widget for ServerAddEditDialog {
                 .as_ref()
                 .map(|s| s.password.clone())
                 .unwrap_or_else(|| "".to_string()),
+            server_type: server
+                .as_ref()
+                .map(|s| s.server_type)
+                .unwrap_or(ServerType::SrvApplication),
         }
     }
 
@@ -72,11 +94,20 @@ impl Widget for ServerAddEditDialog {
                     top_attach: 0,
                 },
             },
+            gtk::CheckButton {
+                label: "Is retired",
+                active: self.model.is_retired,
+                cell: {
+                    left_attach: 0,
+                    top_attach: 1,
+                    width: 2,
+                },
+            },
             gtk::Label {
                 text: "Address:",
                 cell: {
                     left_attach: 0,
-                    top_attach: 1,
+                    top_attach: 2,
                 },
             },
             #[name="address_entry"]
@@ -85,14 +116,14 @@ impl Widget for ServerAddEditDialog {
                 text: &self.model.address,
                 cell: {
                     left_attach: 1,
-                    top_attach: 1,
+                    top_attach: 2,
                 },
             },
             gtk::Label {
                 text: "Text:",
                 cell: {
                     left_attach: 0,
-                    top_attach: 2,
+                    top_attach: 3,
                 },
             },
             #[name="text_entry"]
@@ -101,14 +132,14 @@ impl Widget for ServerAddEditDialog {
                 text: &self.model.text,
                 cell: {
                     left_attach: 1,
-                    top_attach: 2,
+                    top_attach: 3,
                 },
             },
             gtk::Label {
                 text: "Username:",
                 cell: {
                     left_attach: 0,
-                    top_attach: 3,
+                    top_attach: 4,
                 },
             },
             #[name="username_entry"]
@@ -117,14 +148,14 @@ impl Widget for ServerAddEditDialog {
                 text: &self.model.username,
                 cell: {
                     left_attach: 1,
-                    top_attach: 3,
+                    top_attach: 4,
                 },
             },
             gtk::Label {
                 text: "Password:",
                 cell: {
                     left_attach: 0,
-                    top_attach: 4,
+                    top_attach: 5,
                 },
             },
             #[name="password_entry"]
@@ -135,7 +166,22 @@ impl Widget for ServerAddEditDialog {
                 input_purpose: gtk::InputPurpose::Password,
                 cell: {
                     left_attach: 1,
-                    top_attach: 4,
+                    top_attach: 5,
+                },
+            },
+            gtk::Label {
+                text: "Server type:",
+                cell: {
+                    left_attach: 0,
+                    top_attach: 6,
+                },
+            },
+            #[name="server_type"]
+            gtk::ComboBoxText {
+                hexpand: true,
+                cell: {
+                    left_attach: 1,
+                    top_attach: 6,
                 },
             },
         }

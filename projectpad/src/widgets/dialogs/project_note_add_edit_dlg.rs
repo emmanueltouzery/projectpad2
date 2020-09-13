bd1de07dd4659ffba87f34a6d8aeb47274cdce1c
Emmanuel Touzery <etouzery@gmail.com>
2020-09-13 17:54:54 +0200

    add safety pin on the project note add dialog => must select an environment compatible with the project

--- projectpad/src/widgets/dialogs/project_note_add_edit_dlg.rs
+++ projectpad/src/widgets/dialogs/project_note_add_edit_dlg.rs
@@ -9,7 +9,7 @@ use super::standard_dialogs;
 use crate::sql_thread::SqlFunc;
 use diesel::prelude::*;
 use gtk::prelude::*;
-use projectpadsql::models::{EnvironmentType, ProjectNote};
+use projectpadsql::models::{EnvironmentType, Project, ProjectNote};
 use relm::Widget;
 use relm_derive::{widget, Msg};
 use std::sync::mpsc;
@@ -20,6 +20,7 @@ pub enum Msg {
     OkPressed,
     UpdateProjectNote(String),
     ProjectNoteUpdated(ProjectNote),
+    GotProjectEnvironments(environments_picker::SelectedEnvironments),
     EnvironmentToggled(EnvironmentType),
 }
 
@@ -36,6 +37,9 @@ pub struct Model {
     _groups_channel: relm::Channel<Vec<String>>,
     groups_sender: relm::Sender<Vec<String>>,
 
+    _project_environments_channel: relm::Channel<environments_picker::SelectedEnvironments>,
+    project_environments_sender: relm::Sender<environments_picker::SelectedEnvironments>,
+
     _project_note_updated_channel: relm::Channel<SaveResult>,
     project_note_updated_sender: relm::Sender<SaveResult>,
 
@@ -45,6 +49,7 @@ pub struct Model {
     has_uat: bool,
     has_prod: bool,
     group_name: Option<String>,
+    project_environments: Option<environments_picker::SelectedEnvironments>,
     selected_environments: environments_picker::SelectedEnvironments,
     contents: String,
 }
@@ -54,8 +59,17 @@ impl Widget for ProjectNoteAddEditDialog {
     fn init_view(&mut self) {
         dialog_helpers::style_grid(&self.grid);
         self.init_group();
+        self.fetch_project_environments();
         self.grid.set_property_width_request(700);
         self.grid.set_property_height_request(500);
+
+        let no_envs_error_label = gtk::LabelBuilder::new()
+            .label("You must select at least one environment which is active on the parent project")
+            .build();
+        no_envs_error_label.show();
+        self.no_envs_error
+            .get_content_area()
+            .add(&no_envs_error_label);
     }
 
     fn init_group(&self) {
@@ -69,6 +83,25 @@ impl Widget for ProjectNoteAddEditDialog {
             }))
             .unwrap();
         dialog_helpers::init_group_control(&self.model.groups_store, &self.group);
+    }
+
+    fn fetch_project_environments(&self) {
+        let s = self.model.project_environments_sender.clone();
+        let pid = self.model.project_id;
+        self.model
+            .db_sender
+            .send(SqlFunc::new(move |sql_conn| {
+                use projectpadsql::schema::project::dsl as prj;
+                let project: Project = prj::project.find(pid).first(sql_conn).unwrap();
+                s.send(environments_picker::SelectedEnvironments {
+                    has_dev: project.has_dev,
+                    has_stg: project.has_stage,
+                    has_uat: project.has_uat,
+                    has_prod: project.has_prod,
+                })
+                .unwrap();
+            }))
+            .unwrap();
     }
 
     fn model(
@@ -92,6 +125,12 @@ impl Widget for ProjectNoteAddEditDialog {
                 Ok(srv_note) => stream2.emit(Msg::ProjectNoteUpdated(srv_note)),
                 Err((msg, e)) => standard_dialogs::display_error_str(&msg, e),
             });
+        let stream3 = relm.stream().clone();
+        let (project_environments_channel, project_environments_sender) = relm::Channel::new(
+            move |project_environments: environments_picker::SelectedEnvironments| {
+                stream3.emit(Msg::GotProjectEnvironments(project_environments));
+            },
+        );
         let selected_environments = environments_picker::SelectedEnvironments {
             has_dev: pn.map(|d| d.has_dev).unwrap_or(false),
             has_stg: pn.map(|d| d.has_stage).unwrap_or(false),
@@ -108,6 +147,8 @@ impl Widget for ProjectNoteAddEditDialog {
             groups_store: gtk::ListStore::new(&[glib::Type::String]),
             _project_note_updated_channel: project_note_updated_channel,
             project_note_updated_sender,
+            _project_environments_channel: project_environments_channel,
+            project_environments_sender,
             has_dev: pn.map(|d| d.has_dev).unwrap_or(false),
             has_stg: pn.map(|d| d.has_stage).unwrap_or(false),
             has_uat: pn.map(|d| d.has_uat).unwrap_or(false),
@@ -115,6 +156,7 @@ impl Widget for ProjectNoteAddEditDialog {
             title: pn
                 .map(|d| d.title.clone())
                 .unwrap_or_else(|| "".to_string()),
+            project_environments: None,
             selected_environments,
             contents: pn
                 .map(|d| d.contents.clone())
@@ -132,6 +174,9 @@ impl Widget for ProjectNoteAddEditDialog {
                     &groups,
                     &self.model.group_name,
                 );
+            }
+            Msg::GotProjectEnvironments(prj_envs) => {
+                self.model.project_environments = Some(prj_envs);
             }
             Msg::OkPressed => {
                 self.note_edit
@@ -159,14 +204,28 @@ impl Widget for ProjectNoteAddEditDialog {
     }
 
     fn update_project_note(&self, new_contents: String) {
-        let project_id = self.model.project_id;
-        let project_note_id = self.model.project_note_id;
-        let new_title = self.title_entry.get_text();
-        let new_group = self.group.get_active_text();
         let new_has_dev = self.model.has_dev;
         let new_has_stg = self.model.has_stg;
         let new_has_uat = self.model.has_uat;
         let new_has_prod = self.model.has_prod;
+
+        let has_envs = if let Some(prj_envs) = &self.model.project_environments {
+            (prj_envs.has_dev && new_has_dev)
+                || (prj_envs.has_stg && new_has_stg)
+                || (prj_envs.has_uat && new_has_uat)
+                || (prj_envs.has_prod && new_has_prod)
+        } else {
+            false
+        };
+        if !has_envs {
+            self.no_envs_error.set_visible(true);
+            return;
+        }
+
+        let project_id = self.model.project_id;
+        let project_note_id = self.model.project_note_id;
+        let new_title = self.title_entry.get_text();
+        let new_group = self.group.get_active_text();
         let s = self.model.project_note_updated_sender.clone();
         self.model
             .db_sender
@@ -202,12 +261,22 @@ impl Widget for ProjectNoteAddEditDialog {
     view! {
         #[name="grid"]
         gtk::Grid {
+            #[name="no_envs_error"]
+            gtk::InfoBar {
+                message_type: gtk::MessageType::Error,
+                cell: {
+                    left_attach: 0,
+                    top_attach: 0,
+                    width: 2,
+                },
+                visible: false,
+            },
             gtk::Label {
                 text: "Title",
                 halign: gtk::Align::End,
                 cell: {
                     left_attach: 0,
-                    top_attach: 0,
+                    top_attach: 1,
                 },
             },
             #[name="title_entry"]
@@ -216,7 +285,7 @@ impl Widget for ProjectNoteAddEditDialog {
                 text: &self.model.title,
                 cell: {
                     left_attach: 1,
-                    top_attach: 0,
+                    top_attach: 1,
                 },
             },
             gtk::Label {
@@ -224,7 +293,7 @@ impl Widget for ProjectNoteAddEditDialog {
                 halign: gtk::Align::End,
                 cell: {
                     left_attach: 0,
-                    top_attach: 1,
+                    top_attach: 2,
                 },
             },
             #[name="group"]
@@ -232,13 +301,13 @@ impl Widget for ProjectNoteAddEditDialog {
                 hexpand: true,
                 cell: {
                     left_attach: 1,
-                    top_attach: 1,
+                    top_attach: 2,
                 },
             },
             EnvironmentsPicker(self.model.selected_environments.clone()) {
                 cell: {
                     left_attach: 0,
-                    top_attach: 2,
+                    top_attach: 3,
                     width: 2,
                 },
                 EnvironmentsPickerMsgEnvToggled(env_type) => Msg::EnvironmentToggled(env_type)
@@ -247,7 +316,7 @@ impl Widget for ProjectNoteAddEditDialog {
             NoteEdit((self.model.contents.clone(), self.model.accel_group.clone())) {
                 cell: {
                     left_attach: 0,
-                    top_attach: 3,
+                    top_attach: 4,
                     width: 2,
                 },
                 NotePublishContents(ref contents) => Msg::UpdateProjectNote(contents.clone())

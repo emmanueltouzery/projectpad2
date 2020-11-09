03d62c795062176eff51bdc7f7293cb9da26d6ad
Emmanuel Touzery <etouzery@gmail.com>
2020-11-09 19:25:42 +0100

    document that we decided on sqlcipher3 format

--- projectpadsql/src/lib.rs
+++ projectpadsql/src/lib.rs
@@ -29,8 +29,11 @@ pub fn try_unlock_db(db_conn: &SqliteConnection, pass: &str) -> Result<(), Strin
     // https://www.zetetic.net/sqlcipher/sqlcipher-api/#PRAGMA_key
     db_conn
         // https://www.zetetic.net/blog/2018/11/30/sqlcipher-400-release/ on my machine at least, the
-        // GUI app is built with sqlcipher3 and the CLI app with sqlcipher4, so I need these compatability
+        // GUI app is built with sqlcipher3 and the CLI app with sqlcipher4, so I need these compatibility
         // parameters for the sqlcipher4 version to read the DB
+        // I considered using the sqlcipher4 format, but many distributions ship only the sqlcipher3
+        // command-line tools (the latest ubuntu, suse and fedora, as I write this), and these can be
+        // handy for the user.
         .execute(&format!(
             "PRAGMA key='{}'; PRAGMA cipher_page_size = 1024; PRAGMA kdf_iter = 64000; PRAGMA cipher_hmac_algorithm = HMAC_SHA1; PRAGMA cipher_kdf_algorithm = PBKDF2_HMAC_SHA1; SELECT count(*) FROM sqlite_master;",
             &key_escape_param_value(pass)

c7633c523133e2eeadef459fda355d3d1384b344
Emmanuel Touzery <etouzery@gmail.com>
2020-11-21 18:15:34 +0100

    fixes #1 prevent empty passwords in projectpad because moving between encrypted and plain DBs is non-trivial

--- projectpad/src/widgets/dialogs/unlock_db_dlg.rs
+++ projectpad/src/widgets/dialogs/unlock_db_dlg.rs
@@ -24,6 +24,7 @@ pub enum Msg {
 pub struct Model {
     db_sender: mpsc::Sender<SqlFunc>,
     is_new_db: bool,
+    error_label: gtk::Label,
     _pass_valid_channel: relm::Channel<CheckPassResult>,
     pass_valid_sender: relm::Sender<CheckPassResult>,
     password: Option<String>,
@@ -34,13 +35,10 @@ impl Widget for UnlockDbDialog {
     fn init_view(&mut self) {
         dialog_helpers::style_grid(&self.grid);
 
-        let passwords_dont_match_error_label = gtk::LabelBuilder::new()
-            .label("The passwords don't match")
-            .build();
-        passwords_dont_match_error_label.show();
-        self.passwords_dont_match_error
+        self.model.error_label.show();
+        self.error_infobar
             .get_content_area()
-            .add(&passwords_dont_match_error_label);
+            .add(&self.model.error_label);
     }
 
     fn model(relm: &relm::Relm<Self>, params: (bool, mpsc::Sender<SqlFunc>)) -> Model {
@@ -51,10 +49,16 @@ impl Widget for UnlockDbDialog {
         Model {
             db_sender,
             is_new_db,
+            error_label: gtk::LabelBuilder::new().label("").build(),
             pass_valid_sender,
             _pass_valid_channel: pass_valid_channel,
             password: None,
         }
+    }
+
+    fn show_error(&self, msg: &str) {
+        self.model.error_label.set_text(msg);
+        self.error_infobar.set_visible(true);
     }
 
     fn update(&mut self, event: Msg) {
@@ -65,14 +69,21 @@ impl Widget for UnlockDbDialog {
                     .emit(PasswordFieldMsg::RequestPassword);
             }
             Msg::GotPassword(pass) => {
-                self.model.password = Some(pass);
-                self.password_confirm_entry
-                    .stream()
-                    .emit(PasswordFieldMsg::RequestPassword);
+                if pass.is_empty() {
+                    // prevent empty passwords in projectpad2, because moving between
+                    // plaintext and encrypted sqlcipher databases is non-trivial
+                    // https://www.zetetic.net/sqlcipher/sqlcipher-api/index.html#sqlcipher_export
+                    self.show_error("The password must not be empty");
+                } else {
+                    self.model.password = Some(pass);
+                    self.password_confirm_entry
+                        .stream()
+                        .emit(PasswordFieldMsg::RequestPassword);
+                }
             }
             Msg::GotConfirmPassword(pass) => {
                 if self.model.is_new_db && Some(&pass) != self.model.password.as_ref() {
-                    self.passwords_dont_match_error.set_visible(true);
+                    self.show_error("Passwords don't match");
                 } else {
                     let s = self.model.pass_valid_sender.clone();
                     let is_save_to_keyring = self.save_password_check.get_active();
@@ -104,7 +115,7 @@ impl Widget for UnlockDbDialog {
     view! {
         #[name="grid"]
         gtk::Grid {
-            #[name="passwords_dont_match_error"]
+            #[name="error_infobar"]
             gtk::InfoBar {
                 message_type: gtk::MessageType::Error,
                 cell: {

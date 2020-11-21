ea7ff9d8e25da89beed968b7fb87439a98bea4e5
Emmanuel Touzery <etouzery@gmail.com>
2020-11-21 17:46:15 +0100

    refs #1 when checking for an existing DB, also check whether the DB file is empty
    See https://github.com/emmanueltouzery/projectpad2/issues/1
    if you start the app, and close the login screen without
    unlocking the DB, we leave a DB file of zero bytes, and at
    next startup we ask you for the unlock password, we don't
    anymore ask you for a confirm password, because we assume
    there's already a DB around => check that the db file is
    present AND not empty.
    if reading the file length fails, assume a non-empty file.

--- projectpad/src/main.rs
+++ projectpad/src/main.rs
@@ -22,7 +22,24 @@ fn main() {
         process::exit(1);
     }));
 
-    let db_preexisted = projectpadsql::database_path().is_file();
+    let db_path = projectpadsql::database_path();
+
+    // See https://github.com/emmanueltouzery/projectpad2/issues/1
+    // if you start the app, and close the login screen without
+    // unlocking the DB, we leave a DB file of zero bytes, and at
+    // next startup we ask you for the unlock password, we don't
+    // anymore ask you for a confirm password, because we assume
+    // there's already a DB around => check that the db file is
+    // present AND not empty.
+    // if reading the file length fails, assume a non-empty file.
+    let db_preexisted = db_path.is_file()
+        && std::fs::metadata(db_path)
+            .map(|m| m.len())
+            .unwrap_or_else(|e| {
+                eprintln!("Failed reading file metadata? {:?}", e);
+                1
+            })
+            > 0;
 
     let sql_channel = sql_thread::start_sql_thread();
 

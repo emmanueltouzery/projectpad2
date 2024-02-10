a3adfc33810a3659f1fb32eb01ca14508c287f45
Emmanuel Touzery <etouzery@gmail.com>
2024-02-10 20:01:15 +0100

    update the active project in the menu

--- projectpad-gtk4/src/app.rs
+++ projectpad-gtk4/src/app.rs
@@ -113,6 +113,7 @@ impl ProjectpadApplication {
         select_project_action.connect_change_state(move |action, parameter| {
             println!("{} / {:#?}", action, parameter);
             let project_id = parameter.unwrap().get::<i32>().unwrap();
+            action.set_state(parameter.as_ref().unwrap());
             w.set_active_project_and_item(project_id, None);
         });
         window.add_action(&select_project_action);

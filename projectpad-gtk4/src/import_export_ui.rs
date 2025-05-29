f42f205de0a0f63ba545a694cdc2778c75325e84
Emmanuel Touzery <etouzery@gmail.com>
2025-05-29 18:16:52 +0200

    more progress on export

--- projectpad-gtk4/src/import_export_ui.rs
+++ projectpad-gtk4/src/import_export_ui.rs
@@ -1,5 +1,6 @@
 use adw::prelude::*;
-use diesel::prelude::*;
+use diesel::{prelude::*, select};
+use glib::SignalHandlerId;
 use projectpadsql::models::Project;
 
 use crate::widgets::project_items::common;
@@ -80,12 +81,18 @@ pub fn open_import_export_dlg() {
 
     next_btn.connect_clicked(move |btn| {
         if export_radio.is_active() {
-            switch_export_tab(&stack, &btn);
+            btn.set_visible(false);
+            let export_btn = gtk::Button::builder()
+                .label("Export")
+                .css_classes(["suggested-action"])
+                .build();
+            header_bar.pack_end(&export_btn);
+            switch_export_tab(&stack, export_btn);
         }
     });
 }
 
-fn switch_export_tab(stack: &gtk::Stack, next: &gtk::Button) {
+fn switch_export_tab(stack: &gtk::Stack, next: gtk::Button) {
     let import_tab_box = gtk::Box::builder()
         .orientation(gtk::Orientation::Vertical)
         .margin_start(20)
@@ -95,12 +102,20 @@ fn switch_export_tab(stack: &gtk::Stack, next: &gtk::Button) {
         .spacing(10)
         .build();
 
+    let select_all_btn = gtk::Button::builder().label("Select all").build();
     let projects_group = adw::PreferencesGroup::builder()
         .title("Projects to export")
+        .header_suffix(&select_all_btn)
         .build();
+    import_tab_box.append(&projects_group);
+
+    // two preferences groups, because i don't want the title to scroll,
+    // i only want the actual list to scroll. So one group for the title
+    // and one for the contents, within a scrolled window.
+    let projects_group2 = adw::PreferencesGroup::builder().build();
 
     let projects_scroll = gtk::ScrolledWindow::builder()
-        .child(&projects_group)
+        .child(&projects_group2)
         .height_request(300)
         .build();
 
@@ -112,22 +127,56 @@ fn switch_export_tab(stack: &gtk::Stack, next: &gtk::Button) {
             .order(prj::name.asc())
             .load::<Project>(sql_conn)
     }));
-    glib::spawn_future_local(async move {
-        let projects = projects_recv.recv().await.unwrap().unwrap();
-        for project in projects.iter() {
-            projects_group.add(&adw::SwitchRow::builder().title(&project.name).build());
-        }
-    });
+
+    let pass1 = adw::PasswordEntryRow::builder().title("Password").build();
+    let pass2 = adw::PasswordEntryRow::builder()
+        .title("Password confirm")
+        .build();
 
     let password_group = adw::PreferencesGroup::builder().title("Password").build();
-    password_group.add(&adw::PasswordEntryRow::builder().title("Password").build());
-    password_group.add(
-        &adw::PasswordEntryRow::builder()
-            .title("Password confirm")
-            .build(),
-    );
+    password_group.add(&pass1);
+    password_group.add(&pass2);
     import_tab_box.append(&password_group);
 
     stack.add_child(&import_tab_box);
     stack.set_visible_child(&import_tab_box);
+
+    glib::spawn_future_local(async move {
+        let projects = projects_recv.recv().await.unwrap().unwrap();
+        let mut project_rows = vec![];
+        for project in projects.iter() {
+            let project_row = adw::SwitchRow::builder().title(&project.name).build();
+            projects_group2.add(&project_row);
+            project_rows.push(project_row);
+        }
+        let prs = project_rows.clone();
+        select_all_btn.connect_clicked(move |_| {
+            for pr in prs.iter() {
+                pr.set_active(true);
+            }
+        });
+        next.connect_clicked(move |_| {
+            if pass1.text() != pass2.text() {
+                common::simple_error_dlg(
+                    "Export failed",
+                    Some("New and confirm passwords don't match"),
+                );
+                return;
+            }
+            let mut selected_projects = vec![];
+            let mut idx = 0;
+            for project_row in project_rows.iter() {
+                if project_row.is_active() {
+                    selected_projects.push(projects.get(idx).unwrap().id);
+                }
+                idx += 1;
+            }
+            if selected_projects.is_empty() {
+                common::simple_error_dlg(
+                    "Export failed",
+                    Some("No projects were selected for export"),
+                );
+            }
+        });
+    });
 }
